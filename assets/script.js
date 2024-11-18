24163a0f4dd986374070bf5190fbfdf47dcfa957
arthurstinchi <arthur.stinchi@gmail.com>
2024-11-18 08:22:27 -0300

    criando a função de converter e arrumando a formatação

--- assets/script.js
+++ assets/script.js
@@ -3,25 +3,46 @@ let dolar = 5.80
 let usdinput = document.querySelector("#usd")
 let brlinput = document.querySelector("#brl")
 
-usdinput.addEventListener("keyup", () => {
+usdinput.addEventListener("blur", () => {
     usdinput.value = formatCurrency(usdinput.value)
+    convert("usd-to-brl")
 })
 
-brlinput.addEventListener("keyup", () => {
+brlinput.addEventListener("blur", () => {
     brlinput.value = formatCurrency(brlinput.value)
+    convert("brl-to-usd")
 })
 
-usdinput.value = "1000,00"
-convert("usd-to-brl")
+usdinput.addEventListener("keydown",(event) => {
+    if (event.key === "Enter"){
+        usdinput.value = formatCurrency(usdinput.value);
+        convert("usd-to-brl");
+    }
+})
 
+brlinput.addEventListener("keydown",(event) => {
+    if (event.key === "Enter"){
+        brlinput.value = formatCurrency(brlinput.value);
+        convert("brl-to-usd")
+    }
+})
+
+usdinput.addEventListener("input", () => {
+    usdinput.value = usdinput.value.replace(/[^\d,.-]/g, '').replace(",", ".");
+})
+
+brlinput.addEventListener("input", () => {
+    brlinput.value = brlinput.value.replace(/[^\d,.-]/g, '').replace(",", ".");
+})
 
 //funções 
 
 function formatCurrency(value) {
     let fixedValue = fixValue(value)
     let options = {
-        useGrouping: false,
-        minimumFractionDigits: 2, 
+        useGrouping: true,
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2, 
     }
     let formatter = new Intl.NumberFormat("pt-BR", options)
     return formatter.format(fixedValue)
@@ -29,20 +50,30 @@ function formatCurrency(value) {
 }
 
 function fixValue(value){
-    let fixedValue = value.replace(",", ".")
+    let fixedValue = value.replace(/[^\d,.-]/g, '').replace(",",".")
     let floatValue = parseFloat(fixedValue)
-    if(floatValue == NaN){
+    if(isNaN(floatValue)){
         floatValue = 0
     }
     return floatValue
-}
+    }
+
+
 
 function convert(type) {
     if(type == "usd-to-brl"){
+        let fixedValue = fixValue(usdinput.value)
 
+        let result = fixedValue * dolar
+        result = result.toFixed(2)
+        brlinput.value = formatCurrency(result)
     }
     
     if(type == "brl-to-usd"){
+        let fixedValue = fixValue(brlinput.value)
 
+        let result = fixedValue / dolar
+        result = result.toFixed(2)
+        usdinput.value = formatCurrency(result)
     }
 }
