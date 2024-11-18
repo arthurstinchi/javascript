6c48386abce930a7bc0ce13363bf9b4e2c727246
arthurstinchi <arthur.stinchi@gmail.com>
2024-11-17 21:58:09 -0300

    adicionando os primeiros comandos de JS

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+let dolar = 5.80
+
+let usdinput = document.querySelector("#usd")
+let brlinput = document.querySelector("#brl")
+
+usdinput.addEventListener("keyup", () => {
+
+})
+
+brlinput.addEventListener("keyup", () => {
+
+})
+
+usdinput.value = "1000.00"
+convert("usd-to-brl")
+
+
+//funções 
+
+function convert(type) {
+    if(type == "usd-to-brl"){
+
+    }
+    
+    if(type == "brl-to-usd"){
+
+    }
+}
